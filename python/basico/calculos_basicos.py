a85c29c0c61e3b1d8958d681e3b493e96f0613e7
Hamilton Licheski Filho <trabalhohamilton@gmail.com>
2026-02-05 21:53:36 -0300

    calculos_basicos.py

---
+++ python/basico/calculos_basicos.py
@@ -0,0 +1,12 @@
+# Exercício: Média Aritmética
+# Objetivo: ler duas notas de um aluno e calcular a média final
+
+# Entrada de dados
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+# Processamento
+media = (nota1 + nota2) / 2
+
+# Saída de dados
+print(f'A média das notas é {media:.2f}')
